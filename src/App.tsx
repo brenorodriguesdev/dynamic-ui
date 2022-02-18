201ad7e8aa1500f0bd564c1ab9b1db40a7338247
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-02-17 23:23:21 -0300

    feat: add toast container

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ import Controllers from "./pages/controllers";
 import CreateController from "./pages/create-controller";
 import CreateService from "./pages/create-service";
 import Services from "./pages/services";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Controllers />} />
@@ -19,7 +20,7 @@ export default function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/createService" element={<CreateService />} />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
-
   )
 }
