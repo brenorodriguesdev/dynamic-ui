97c6ffd272b5c7c02d2cb312527f9866c4188472
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-02-17 23:00:56 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Controllers from "./pages/controllers";
+import CreateController from "./pages/create-controller";
+import CreateService from "./pages/create-service";
+import Services from "./pages/services";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Controllers />} />
+        <Route path="/controllers" element={<Controllers />} />
+        <Route path="/createController" element={<CreateController />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/createService" element={<CreateService />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
